23b8de22d46c0ab2f2e847d745ebbcf074dc50a2
samengulsan <dengulsun9@gmail.com>
2025-10-22 14:52:16 +0530

    created 2 endoints to get roles and aff-org &  changed status codefrom 403 to 401.

--- backend/phagematchapi/urls.py
+++ backend/phagematchapi/urls.py
@@ -1,11 +1,13 @@
 # users/urls.py
 from django.urls import path
-from .views import RegisterView, LoginView, NonAdminUserDetailView, NonAdminUserListView, ApproveUserView
+from .views import RegisterView, LoginView, NonAdminUserDetailView, NonAdminUserListView, ApproveUserView,RolesView, AffiliatedOrganizationView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='user-register'),
     path('login/', LoginView.as_view(), name='user-login'),
     path('users/', NonAdminUserListView.as_view(), name='nonadmin-users-list'),
     path('users/<int:pk>/', NonAdminUserDetailView.as_view(), name='nonadmin-user-detail'),
-     path('users/<int:pk>/approve/', ApproveUserView.as_view(), name='approve-user'),
+    path('users/<int:pk>/approve/', ApproveUserView.as_view(), name='approve-user'),
+    path('roles/', RolesView.as_view(), name='roles-list'),
+    path('afforg/', AffiliatedOrganizationView.as_view(), name='affiliated-organizations-list'),
 ]
